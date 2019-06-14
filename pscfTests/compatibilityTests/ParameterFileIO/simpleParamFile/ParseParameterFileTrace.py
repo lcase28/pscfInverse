8d61f22327f20ea79eb4d8653864f5e68b858c55
LoganCase <case0234@umn.edu>
2019-06-14 14:49:26 -0500

    Further Reorganization of project repository

--- pscfTests/compatibilityTests/ParameterFileIO/simpleParamFile/ParseParameterFileTrace.py
+++ pscfTests/compatibilityTests/ParameterFileIO/simpleParamFile/ParseParameterFileTrace.py
@@ -1,6 +1,8 @@
-from .....ParamFactories import *
+#from ..ParamFactories import *
+from context import psoinverse
+from psoinverse import ParamFactories as pf
 
-pfac = PSCFPPFactory()
+pfac = pf.PSCFPPFactory()
 
 #fname = "/Users/logancase/Documents/Research/PSCF/CPP_Update/examples/fd1d/cylindrical/1/param"
 #fname = "/Users/logancase/Documents/Research/PSO/Development/Primary/psoinverse/params_HEXortho_template.in"
