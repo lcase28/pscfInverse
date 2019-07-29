ec27af77e662d3cee2840a20c4618761362323a8
LoganCase <case0234@umn.edu>
2019-07-29 15:20:35 -0500

    Starting implementation of Field Generation for 3-dimensions (attempting to generalize).

--- psoinverse/mesophases/FieldGenerators.py
+++ psoinverse/mesophases/FieldGenerators.py
@@ -1,12 +1,44 @@
 # Imports
 from abc import ABC, abstractmethod
+from crystals import Lattice, LatticeSystem
 import numpy as np
 
-class FieldGenerator(ABC):
+#class FieldGenerator(ABC):
+#    """ Abstract base class for field generators """
+#    
+#    def __init__(**kwargs):
+#        lat = kwargs.get("lattice", None)
+#        if lat is not None:
+#            self.lattice = 
+#        super().__init(**kwargs)
+#    
+#    @abstractmethod
+#    def WriteFile(**kwargs):
+#        pass
+#    
+#    def DensityFields(self, **kwargs):
     
-    def __init__(**kwargs):
-        super().__init(**kwargs)
+
+class FieldGenerator(object):
+    """ Generator class for 3D k-grid density fields of diblock systems """
     
-    @abstractmethod
-    def WriteFields(**kwargs):
-        pass
+    def __init__(self, lattice, particlePositions, **kwargs):
+        self.lattice = lattice
+        super().__init__(**kwargs)
+    
+    def to_kgrid(self, ngrid):
+        
+        for (i,x) in enumerate(ngrid):
+            if i == 0:
+                ngrid[i] = x/2
+            else:
+                ngrid[i] = x - 1
+            
+        nwaves = np.prod(ngrid)
+        field
+        
+        # primary loop for n-dimensional generation
+        for G in enumerate(itertools.product(*[range(x) for x in ngrid])):
+            # fill in loop operations - G is miller indices in n-dimensions.
+            
+    
--- psoinverse/mesophases/Lattice.py
+++ psoinverse/mesophases/Lattice.py
@@ -1,11 +1,59 @@
+# Lattice Module. 
+# Partially derived from crystals package available at:
+#   https://github.com/LaurentRDC/crystals.git
+# Generallized to handle 1-, 2-, or 3-dimensional systems
+
 # Imports
+from abc import ABC, abstractmethod
 import numpy as np
 
-class Lattice(object):
+class Lattice(ABC):
+    """ Abstract base class of n-dimensional lattices """
+    
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+    
+    @abstractmethod
+    def __repr__(self):
+        return NotImplemented
+    
+    @abstractmethod
+    def __hash__(self):
+        return super().hash()
+    
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            reutrn np.allclose(self.lattice_vectors, other.lattice_vectors, atol=1e-4)
+        return NotImplemented
+    
+    @abstractmethod
+    def __array__(self, *args, **kwargs):
+        pass
+    
+class Lattice3D(Lattice):
     """ Object representing a crystallographic basis vector lattice"""
     
     def __init__(self, basis, **kwargs):
-        self.a, self.b, self.c, self.alpha, self.beta, self.gamma = basis
+        a1, a2, a3 = basis
+        self.a1 = np.asarray(a1, dtype=np.float)
+        self.a2 = np.asarray(a2, dtype=np.float)
+        self.a3 = np.asarray(a3, dtype=np.float)
         super().__init__(**kwargs)
+    
+    def __repr__(self):
+        s = "< Lattice object with parameters {:.3f}A, {:.3f}, {:.3f}, {:.3f}, {:.3f} >"
+        return s.format(*self.lattice_parameters)
+    
+    def __hash__(self):
+        return hash(self.lattice_parameters) | super().hash()
+        
+    def __array__(self, *args, **kwargs):
+        """ returns a 3x3 float array. Each row is a lattice vector. """
+        return np.array(self.lattice_vectors, *args, *kwargs)
+    
+    @classmethod
+    def from_parameters(cls, a, b, c, alpha, beta, gamma):
+        
+    
     
     
