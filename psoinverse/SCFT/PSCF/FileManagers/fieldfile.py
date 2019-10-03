9445890b4c2b2c13c7d1111e361902093b4d5265
LoganCase <case0234@umn.edu>
2019-10-03 14:11:57 -0500

    Modification of PSCF FieldFile class to handle coordinate and wave-vector grid formats in addition to symmetry-adapted formats. Header data now read within abstract base class, and field data read implemented by inheriting classes.

---
+++ psoinverse/SCFT/PSCF/FileManagers/fieldfile.py
@@ -0,0 +1,581 @@
+from abc import ABC, AbstractMethod
+from io import IO, IoException
+from version import Version
+import string
+import sys
+
+class FieldFile(ABC):
+    '''
+    Hold data in a PSCF field file. 
+
+    A FieldFile object contains the data in a field file in the PSCF
+    symmetry-adapted Fourier expansion format (see web user manual).
+    It can be used to represent either omega (chemical potential) or
+    rho (volume fraction) fields. a
+
+    The constructor reads a field file, creates a FieldFile object to store
+    the data, and stores all of the parameters and field coefficients in 
+    attributes of the object.
+
+    Attributes:
+    dim            -- [int] number of periodic spatial directions
+    crystal_system -- [string] label of Bravais crystal system
+    N_cell_param   -- [int] number of unit cell parameters
+    cell_param     -- [list] list of real unit cell parameters
+    group          -- [string] name of space group
+    N_monomer      -- [int] number of monomer types
+    N_star         -- [int] number of basis functions (or stars)
+    fields         -- [list] list of list of coefficients
+
+    The attribute field[i] is is a list (or array) of coefficients
+    for a field associated with monomer type number i. The element
+    field[i][j] is the coefficient of basis function j in the field
+    associated with monomer type i. 
+    '''
+
+    # "Public" methods
+
+    def __init__(self,filename):
+        '''
+        Read a PSCF symmetry-adapted field file, and create a new object.
+
+        Argument:
+        filename -- name of file
+
+        The file named filename is opened and closed within this function.
+        '''
+        self.file = open(filename, 'r')
+	self._io   = IO()
+	file = self.file
+
+        # Read version line
+	self.version = Version(self.file)
+
+        self._input_unit_cell()
+        self.group_name = self._input_var('char')
+        self.N_monomer = self._input_var('int')
+        self._readField();
+        #self.N_star = self._input_var('int')
+
+        ## Define empty lists
+        #self.fields = []
+        #self.waves = []
+        #self.counts = []
+
+        #for i in range(self.N_star):
+        #    data = file.readline().split()
+        #    if len(data) != self.N_monomer + self.dim + 1:
+        #        raise IoException('Incorrect number of elements in field line')
+        #    j = 0
+
+        #    # Read field coefficients
+        #    self.fields.append([])
+        #    for k in range(self.N_monomer):
+        #        value = float(data[j])
+        #        self.fields[i].append(value)
+        #        j += 1
+
+        #    # Read field coefficients
+        #    self.waves.append([])
+        #    for k in range(self.dim):
+        #        value = int(data[j])
+        #        self.waves[i].append(value)
+        #        j += 1
+
+        #    # Read star_count
+        #    self.counts.append(int(data[j]))
+
+        self.file.close()
+	self.file = None
+
+    def write(self, file, major=1, minor=0):
+        '''
+        PURPOSE
+           Write field to file in PSCF symmetry-adapted format.
+        ARGUMENTS
+           file  - file object or file name string
+           major - major file format version number
+           minor - minor file format version number
+        COMMENT
+           if file is a field object, it must be open for writing
+        '''
+
+        # If file argument is a string, open a file of that name
+        if type(file) == type('thing'):
+            temp = open(file,'w')
+            file = temp
+        self.file = file
+           
+	self.version.major = major
+	self.version.minor = minor
+	self.version.write(file)
+
+        self._output_unit_cell()
+        self._output_var('char', 'group_name')
+        self._output_var( 'int', 'N_monomer')
+        self._outputField()
+        #self._output_var( 'int', 'N_star')
+
+        #for i in range(self.N_star):
+        #    for k in range(self.N_monomer):
+        #        file.write('%20.12E' % self.fields[i][k])
+        #    file.write('    ')
+        #    for k in range(self.dim):
+        #        file.write('%4d' % self.waves[i][k])
+        #    file.write('%6d' % self.counts[i])
+        #    file.write("\n")
+
+	file.close()
+        self.file = None
+    
+    @AbstractMethod
+    def addMonomer(self):
+        ''' 
+        PURPOSE
+           Add a field with a coefficients set to common value
+        ARGUMENTS
+           value - value of all coefficients for new momoner
+        COMMENT
+            N_momomer is incremented by 1.
+            Inheriting class must define update of the field variables
+        '''
+        self.N_monomer += 1
+    
+    @AbstractMethod
+    def duplicateMonomer(self, i):
+        ''' 
+        PURPOSE
+           Add a field by duplicating field i
+        ARGUMENTS
+           i - index in range [0, N_monomer-1]
+        COMMENT
+            N_momomer is incremented.
+            Inheriting class must define update of field variable.
+        '''
+        self.N_monomer += 1
+    
+    @AbstractMethod
+    def switchMonomers(self, i, j):
+        '''
+        PURPOSE
+           Switch coefficients of fields i and j
+        ARGUMENTS
+           i - index in range [0, N_monomer-1]
+           j - index in range [0, N_monomer-1]
+        '''
+        pass
+
+    # "Private" methods
+    
+    # Abstract Methods
+    
+    @AbstractMethod
+    def _readField(self):
+        pass
+    
+    @AbstractMethod
+    def _outputField(self):
+        pass
+
+    # Wrappers for input_... output_.. methods of IO)
+
+    def _input_var(self, type, comment = None, f='A'):
+        return self._io.input_var(self.file, type, comment, f)
+
+    def _input_vec(self, type, n=None, comment=None, s='R',f='A'):
+        return self._io.input_vec(self.file, type, n, comment, s, f)
+
+    # Output methods (output by name)
+    def _output_var(self, type, name, f='A'):
+        if self.__dict__.has_key(name):
+            data = self.__dict__[name]
+	    self._io.output_var(self.file, type, data, name, f)
+
+    def _output_vec(self, type, name, n=None, s='R', f='A'):
+        if self.__dict__.has_key(name):
+            data = self.__dict__[name]
+	    self._io.output_vec(self.file, type, data, n, name, s, f)
+
+    def _input_unit_cell(self):
+        ''' Analog of subroutine _input_unit_cell in unit_cell_mod.f '''
+        self.dim = self._input_var('int')
+        self.crystal_system = self._input_var('char')
+        self.N_cell_param = self._input_var('int')
+        self.cell_param = self._input_vec('real',self.N_cell_param)
+
+    def _output_unit_cell(self):
+        ''' Analog of subroutine _output_unit_cell in unit_cell_mod.f '''
+        self._output_var('int', 'dim')
+        self._output_var('char', 'crystal_system')
+        self._output_var('int', 'N_cell_param')
+        self._output_vec('real', 'cell_param', self.N_cell_param)
+
+class SymFieldFile(FieldFile):
+    '''
+    Hold data in a PSCF field file. 
+
+    A FieldFile object contains the data in a field file in the PSCF
+    symmetry-adapted Fourier expansion format (see web user manual).
+    It can be used to represent either omega (chemical potential) or
+    rho (volume fraction) fields. a
+
+    The constructor reads a field file, creates a FieldFile object to store
+    the data, and stores all of the parameters and field coefficients in 
+    attributes of the object.
+
+    Attributes:
+    dim            -- [int] number of periodic spatial directions
+    crystal_system -- [string] label of Bravais crystal system
+    N_cell_param   -- [int] number of unit cell parameters
+    cell_param     -- [list] list of real unit cell parameters
+    group          -- [string] name of space group
+    N_monomer      -- [int] number of monomer types
+    N_star         -- [int] number of basis functions (or stars)
+    fields         -- [list] list of list of coefficients
+
+    The attribute field[i] is is a list (or array) of coefficients
+    for a field associated with monomer type number i. The element
+    field[i][j] is the coefficient of basis function j in the field
+    associated with monomer type i. 
+    '''
+
+    # "Public" methods
+
+    def __init__(self,filename):
+        '''
+        Read a PSCF symmetry-adapted field file, and create a new object.
+
+        Argument:
+        filename -- name of file
+
+        The file named filename is opened and closed within this function.
+        '''
+        super().__init__(filename)
+
+    def write(self, file, major=1, minor=0):
+        '''
+        PURPOSE
+           Write field to file in PSCF symmetry-adapted format.
+        ARGUMENTS
+           file  - file object or file name string
+           major - major file format version number
+           minor - minor file format version number
+        COMMENT
+           if file is a field object, it must be open for writing
+        '''
+        super().write(fiel, major, minor)
+
+    def addMonomer(self, value = 0.0, **kwargs):
+        ''' 
+        PURPOSE
+           Add a field with a coefficients set to common value
+        ARGUMENTS
+           value - value of all coefficients for new momoner
+        COMMENT
+            N_momomer is incremented by 1, and new field is last
+        '''
+        super().addMonomer(**kwargs)
+        for k in range(self.N_star):
+            self.fields[k].append(value)
+
+    def duplicateMonomer(self, i):
+        ''' 
+        PURPOSE
+           Add a field by duplicating field i
+        ARGUMENTS
+           i - index in range [0, N_monomer-1]
+        COMMENT
+            N_momomer is incremented, and duplicate field is last
+        '''
+        super().duplicateMonomer(i)
+        for k in range(self.N_star):
+            self.fields[k].append(self.fields[k][i])
+
+    def switchMonomers(self, i, j):
+        '''
+        PURPOSE
+           Switch coefficients of fields i and j
+        ARGUMENTS
+           i - index in range [0, N_monomer-1]
+           j - index in range [0, N_monomer-1]
+        '''
+        for k in range(self.N_star):
+            temp = self.fields[k][i]
+            self.fields[k][i] = self.fields[k][j]
+            self.fields[k][j] = temp
+
+    # "Private" methods
+    
+    def _readField(self):
+        self.N_star = self._input_var('int')
+
+        # Define empty lists
+        self.fields = []
+        self.waves = []
+        self.counts = []
+
+        for i in range(self.N_star):
+
+            data = file.readline().split()
+            if len(data) != self.N_monomer + self.dim + 1:
+                raise IoException('Incorrect number of elements in field line')
+            j = 0
+
+            # Read field coefficients
+            self.fields.append([])
+            for k in range(self.N_monomer):
+                value = float(data[j])
+                self.fields[i].append(value)
+                j += 1
+
+            # Read field coefficients
+            self.waves.append([])
+            for k in range(self.dim):
+                value = int(data[j])
+                self.waves[i].append(value)
+                j += 1
+
+            # Read star_count
+            self.counts.append(int(data[j]))
+    
+    def _writeField(self):
+        self._output_var( 'int', 'N_star')
+        for i in range(self.N_star):
+            for k in range(self.N_monomer):
+                file.write('%20.12E' % self.fields[i][k])
+            file.write('    ')
+            for k in range(self.dim):
+                file.write('%4d' % self.waves[i][k])
+            file.write('%6d' % self.counts[i])
+            file.write("\n")
+    
+    # Wrappers for input_... output_.. methods of IO)
+
+    def _input_var(self, type, comment = None, f='A'):
+        return self._io.input_var(self.file, type, comment, f)
+
+    def _input_vec(self, type, n=None, comment=None, s='R',f='A'):
+        return self._io.input_vec(self.file, type, n, comment, s, f)
+
+    # Output methods (output by name)
+    def _output_var(self, type, name, f='A'):
+        if self.__dict__.has_key(name):
+            data = self.__dict__[name]
+	    self._io.output_var(self.file, type, data, name, f)
+
+    def _output_vec(self, type, name, n=None, s='R', f='A'):
+        if self.__dict__.has_key(name):
+            data = self.__dict__[name]
+	    self._io.output_vec(self.file, type, data, n, name, s, f)
+
+    def _input_unit_cell(self):
+        ''' Analog of subroutine _input_unit_cell in unit_cell_mod.f '''
+        self.dim = self._input_var('int')
+        self.crystal_system = self._input_var('char')
+        self.N_cell_param = self._input_var('int')
+        self.cell_param = self._input_vec('real',self.N_cell_param)
+
+    def _output_unit_cell(self):
+        ''' Analog of subroutine _output_unit_cell in unit_cell_mod.f '''
+        self._output_var('int', 'dim')
+        self._output_var('char', 'crystal_system')
+        self._output_var('int', 'N_cell_param')
+        self._output_vec('real', 'cell_param', self.N_cell_param)
+
+class CoordFieldFile(FieldFile):
+    '''
+    Hold data in a PSCF field file. 
+
+    A FieldFile object contains the data in a field file in the PSCF
+    symmetry-adapted Fourier expansion format (see web user manual).
+    It can be used to represent either omega (chemical potential) or
+    rho (volume fraction) fields. a
+
+    The constructor reads a field file, creates a FieldFile object to store
+    the data, and stores all of the parameters and field coefficients in 
+    attributes of the object.
+
+    Attributes:
+    dim            -- [int] number of periodic spatial directions
+    crystal_system -- [string] label of Bravais crystal system
+    N_cell_param   -- [int] number of unit cell parameters
+    cell_param     -- [list] list of real unit cell parameters
+    group          -- [string] name of space group
+    N_monomer      -- [int] number of monomer types
+    N_star         -- [int] number of basis functions (or stars)
+    fields         -- [list] list of list of coefficients
+
+    The attribute field[i] is is a list (or array) of coefficients
+    for a field associated with monomer type number i. The element
+    field[i][j] is the coefficient of basis function j in the field
+    associated with monomer type i. 
+    '''
+
+    # "Public" methods
+
+    def __init__(self,filename):
+        '''
+        Read a PSCF symmetry-adapted field file, and create a new object.
+
+        Argument:
+        filename -- name of file
+
+        The file named filename is opened and closed within this function.
+        '''
+        self.file = open(filename, 'r')
+	self._io   = IO()
+	file = self.file
+
+        # Read version line
+	self.version = Version(self.file)
+
+        self._input_unit_cell()
+        self.group_name = self._input_var('char')
+        self.N_monomer = self._input_var('int')
+        self.N_star = self._input_var('int')
+
+        # Define empty lists
+        self.fields = []
+        self.waves = []
+        self.counts = []
+
+        for i in range(self.N_star):
+
+            data = file.readline().split()
+            if len(data) != self.N_monomer + self.dim + 1:
+                raise IoException('Incorrect number of elements in field line')
+            j = 0
+
+            # Read field coefficients
+            self.fields.append([])
+            for k in range(self.N_monomer):
+                value = float(data[j])
+                self.fields[i].append(value)
+                j += 1
+
+            # Read field coefficients
+            self.waves.append([])
+            for k in range(self.dim):
+                value = int(data[j])
+                self.waves[i].append(value)
+                j += 1
+
+            # Read star_count
+            self.counts.append(int(data[j]))
+
+        self.file.close()
+	self.file = None
+
+    def write(self, file, major=1, minor=0):
+        '''
+        PURPOSE
+           Write field to file in PSCF symmetry-adapted format.
+        ARGUMENTS
+           file  - file object or file name string
+           major - major file format version number
+           minor - minor file format version number
+        COMMENT
+           if file is a field object, it must be open for writing
+        '''
+
+        # If file argument is a string, open a file of that name
+        if type(file) == type('thing'):
+            temp = open(file,'w')
+            file = temp
+        self.file = file
+           
+	self.version.major = major
+	self.version.minor = minor
+	self.version.write(file)
+
+        self._output_unit_cell()
+        self._output_var('char', 'group_name')
+        self._output_var( 'int', 'N_monomer')
+        self._output_var( 'int', 'N_star')
+
+        for i in range(self.N_star):
+            for k in range(self.N_monomer):
+                file.write('%20.12E' % self.fields[i][k])
+            file.write('    ')
+            for k in range(self.dim):
+                file.write('%4d' % self.waves[i][k])
+            file.write('%6d' % self.counts[i])
+            file.write("\n")
+
+	file.close()
+        self.file = None
+
+    def addMonomer(self, value = 0.0):
+        ''' 
+        PURPOSE
+           Add a field with a coefficients set to common value
+        ARGUMENTS
+           value - value of all coefficients for new momoner
+        COMMENT
+            N_momomer is incremented by 1, and new field is last
+        '''
+        self.N_monomer += 1
+        for k in range(self.N_star):
+            self.fields[k].append(value)
+
+    def duplicateMonomer(self, i):
+        ''' 
+        PURPOSE
+           Add a field by duplicating field i
+        ARGUMENTS
+           i - index in range [0, N_monomer-1]
+        COMMENT
+            N_momomer is incremented, and duplicate field is last
+        '''
+        self.N_monomer += 1
+        for k in range(self.N_star):
+            self.fields[k].append(self.fields[k][i])
+
+    def switchMonomers(self, i, j):
+        '''
+        PURPOSE
+           Switch coefficients of fields i and j
+        ARGUMENTS
+           i - index in range [0, N_monomer-1]
+           j - index in range [0, N_monomer-1]
+        '''
+        for k in range(self.N_star):
+            temp = self.fields[k][i]
+            self.fields[k][i] = self.fields[k][j]
+            self.fields[k][j] = temp
+
+    # "Private" methods
+
+    # Wrappers for input_... output_.. methods of IO)
+
+    def _input_var(self, type, comment = None, f='A'):
+        return self._io.input_var(self.file, type, comment, f)
+
+    def _input_vec(self, type, n=None, comment=None, s='R',f='A'):
+        return self._io.input_vec(self.file, type, n, comment, s, f)
+
+    # Output methods (output by name)
+    def _output_var(self, type, name, f='A'):
+        if self.__dict__.has_key(name):
+            data = self.__dict__[name]
+	    self._io.output_var(self.file, type, data, name, f)
+
+    def _output_vec(self, type, name, n=None, s='R', f='A'):
+        if self.__dict__.has_key(name):
+            data = self.__dict__[name]
+	    self._io.output_vec(self.file, type, data, n, name, s, f)
+
+    def _input_unit_cell(self):
+        ''' Analog of subroutine _input_unit_cell in unit_cell_mod.f '''
+        self.dim = self._input_var('int')
+        self.crystal_system = self._input_var('char')
+        self.N_cell_param = self._input_var('int')
+        self.cell_param = self._input_vec('real',self.N_cell_param)
+
+    def _output_unit_cell(self):
+        ''' Analog of subroutine _output_unit_cell in unit_cell_mod.f '''
+        self._output_var('int', 'dim')
+        self._output_var('char', 'crystal_system')
+        self._output_var('int', 'N_cell_param')
+        self._output_vec('real', 'cell_param', self.N_cell_param)
+
---
+++ psoinverse/SCFT/PSCF/FileManagers/sweep.py
@@ -0,0 +1,91 @@
+from outfile import OutFile
+import sys, os, string
+
+class Sweep(object):
+    """
+    Contains data in all summary output files produced by a PSCF sweep.
+
+    The constructor reads in, parses and stores all of the files with
+    names of the form i.out in a directory, where i is an integer in
+    a sequence range [0, n]. 
+
+    The class emulates a list of Outfile objects: If s is a Sweep object 
+    constructed from directory 'dir', then s[i] returns the OutFile object 
+    constructed by parsing the file 'dir/i.out' , where i is an integer.
+
+    Implementation: Use of the [] subscript operator is implemented by 
+    overloading the __getitem__ method.
+    """
+   
+    def __init__(self, directory):
+        """
+        Create a Sweep object by reading all output files in a directory.
+
+        Arguments:
+           directory -- path name for directory [string]
+        """
+        old_cwd = os.getcwd()
+        os.chdir(directory)
+
+        # Find filenames of *.out files
+        filenames = {}
+        for file in os.listdir('.'):
+            if file.endswith(r".out"):
+                filenames[file[:-4]] = file
+        n = len(filenames)
+
+        # Make list self._outfiles of OutFile objects
+        self._outfiles = []
+        i = 0
+        consecutive = True
+        while i < n and consecutive:
+            key = str(i)
+            if key in filenames.keys():
+                filename = filenames[key]
+                self._outfiles.append(OutFile(filename))
+                i = i + 1
+            else:
+                consecutive = False
+
+        # Return to original current working directory
+        os.chdir(old_cwd)
+
+    def write(self, expr1, expr2):
+        """
+        Return a two column string containing values of python expressions.
+
+        The arguments expr1 and expr2 are python expressions with 
+        numerical (integer or real) values that are constructed using 
+        the names of attributes of individual OutFile objects as variables. 
+        The value of each expression is evaluated using the eval method of
+        the ParamFile class, which is inherited by OutFile. 
+
+        This class is intended to make it easy to plot output pairs of
+        values in a form suitable for plotting or further analysis. The
+        simplest application is to give expr1 (the first column) as the
+        value of some control parameter that is varied in the sweep, and
+        expr2 (the second column) as f_Helmholtz, the Helmholtz free 
+        energy per monomer.
+        """
+        s = []
+        for i in range(len(self._outfiles)):
+            if self[i]:
+                # Make local variables: For each attribute 
+                # x[i].key of OutFile object self[i], make a
+                # corresponding local variable named "key", 
+                # by executing: key = self[i].key
+                for key in self[i].__dict__.keys():
+                    exec( key + '= self[i].' + key )
+                # Print results of expressions expr1 and expr2
+                line = str(eval(expr1)) +  '   ' + str(eval(expr2)) 
+                s.append(line)
+        return string.join(s,"\n")
+
+    def __getitem__(self,i):
+        """
+        Allow a Sweep to emulate a list of OutFile objects.
+        """
+        return self._outfiles[i]
+    
+    def __len__(self):
+        return len(self._outfiles)
